45857ec5fcb1a5412da440f00053c95b61e64db1
kenneth <kennethkimken@gmail.com>
2022-11-06 22:31:44 +0300

    Updates

---
+++ precache-manifest.aa79e4ea7bd50127f24c60b0b3915272.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "923c8809efc8ce0fce02",
+    "url": "/barber-shop-/static/css/main.8dc6d2b6.chunk.css"
+  },
+  {
+    "revision": "923c8809efc8ce0fce02",
+    "url": "/barber-shop-/static/js/main.923c8809.chunk.js"
+  },
+  {
+    "revision": "599995f5c63098e6b233",
+    "url": "/barber-shop-/static/js/1.599995f5.chunk.js"
+  },
+  {
+    "revision": "9c0fcdb1cde5ee86c48a",
+    "url": "/barber-shop-/static/css/2.5e0ed670.chunk.css"
+  },
+  {
+    "revision": "9c0fcdb1cde5ee86c48a",
+    "url": "/barber-shop-/static/js/2.9c0fcdb1.chunk.js"
+  },
+  {
+    "revision": "de49b1c226d95747e3e2",
+    "url": "/barber-shop-/static/js/runtime~main.de49b1c2.js"
+  },
+  {
+    "revision": "f0ef3a578d3afdd8466957b23fc6b514",
+    "url": "/barber-shop-/static/media/Clogo.f0ef3a57.png"
+  },
+  {
+    "revision": "af09c82de181d3934462550e0e70ee65",
+    "url": "/barber-shop-/index.html"
+  }
+];
